0f726e1cf024fe10c52d0383b2fd316811bde67a
Damian Pagowski <d.pagowski@gmail.com>
2020-05-04 17:44:32 +0700

    updated port

--- app.py
+++ app.py
@@ -3,10 +3,6 @@ from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
-
-
-port = int(os.environ.get('SERVER_PORT',3030))
-
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
@@ -24,4 +20,5 @@ def hello():
 
 
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
     APP.run(host='0.0.0.0', port=port, debug=True)
