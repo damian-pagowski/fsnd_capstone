869f3eab94ad968b3e81bd1545f10b13d8403ebe
Damian Pagowski <d.pagowski@gmail.com>
2020-05-04 17:51:33 +0700

    testin deployment without app.json

--- app.py
+++ app.py
@@ -7,17 +7,13 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
     CORS(app)
-
     return app
 
-
 APP = create_app()
-
 
 @APP.route('/', methods=['GET'])
 def hello():
     return jsonify({"status": "ok"})
-
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
